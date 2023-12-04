686a611cd21cc6ec00206e0d499dd13ada562914
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-12-04 21:38:39 +0900

    Test: 최종 우승자를 반환하는 기능 테스트 추가

--- __tests__/domain/CarTest.js
+++ __tests__/domain/CarTest.js
@@ -21,4 +21,24 @@ describe('Car 클래스 테스트', () => {
       },
     );
   });
+
+  describe(`findWinner 메서드는 max를 입력받아 자동차의 position과 max를 비교하여 자동차의 이름을 반환한다.`, () => {
+    const cases = [
+      { carName: 'a', randomNumber: 4, max: 1, expected: 'a' },
+      { carName: 'b', randomNumber: 6, max: 0, expected: 'b' },
+      { carName: 'c', randomNumber: 1, max: 3, expected: undefined },
+    ];
+
+    test.each(cases)(
+      '자동차들의 이동의 최대값인 $max가 주어지는 경우, findWinner()는 position값이 $max보다 큰 경우 자동차의 이름을 반환한다.',
+      ({ carName, randomNumber, max, expected }) => {
+        // when
+        const car = new Car(carName);
+        car.move(randomNumber);
+
+        // then
+        expect(car.findWinner(max)).toEqual(expected);
+      },
+    );
+  });
 });
